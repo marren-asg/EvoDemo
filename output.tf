121a2888da17a7d273d3fb9dcc6b8efae8653ae4
Markus Renström <93190711+marren-asg@users.noreply.github.com>
2022-03-18 11:45:31 +0100

    Update output.tf
    Added some more functionality ;)

--- output.tf
+++ output.tf
@@ -1,9 +1,18 @@
 /*output "ipaddres" {
       description = "Public IP for the VM: "
       value = azurerm_public_ip.testip.ip_address
-}*/
-
+}
 
 output "public_ip_address" {
   value = azurerm_public_ip.testip.*.ip_address
+}*/
+      
+output "vm_ids" {
+  description = "Virtual machine name."
+  value       =  azurerm_linux_virtual_machine.main.name
+}
+
+output "vm_location" {
+  description = "Virtual machine location."
+  value       =  azurerm_linux_virtual_machine.main.location
 }
